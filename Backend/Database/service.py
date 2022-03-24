6a68edf75b22bce6054d8fc5c1ae8bd669d0e17f
Eskiel Getachew <egetachew@uh.edu>
2022-03-24 14:37:39 -0500

    Service Table Fixes

--- Backend/Database/service.py
+++ Backend/Database/service.py
@@ -8,36 +8,36 @@ class ServiceDB:
 
 
     def getAllService(self):
-        self.cursor.execute("SELECT * FROM service WHERE active = 1")
+        self.cursor.execute("SELECT * FROM service")
         rows = self.cursor.fetchall()
         return rows
 
 
     def getServiceByServiceID(self, service_id):
-        self.cursor.execute("SELECT * FROM service WHERE service_id = ? AND active = 1", (account_id,))
+        self.cursor.execute("SELECT * FROM service WHERE service_id = ?", (service_id,))
         rows = self.cursor.fetchall()
         return rows
 
 
     def getServiceByServiceDescription(self, service_description):
-        self.cursor.execute("SELECT * FROM service WHERE service_description = ? AND active = 1", (username,))
+        self.cursor.execute("SELECT * FROM service WHERE service_description = ?", (service_description,))
         rows = self.cursor.fetchall()
         return rows
 
     def getServiceByPrice(self, price):
-        self.cursor.execute("SELECT * FROM service WHERE price = ? AND active = 1", (username,))
+        self.cursor.execute("SELECT * FROM service WHERE price = ?", (price,))
         rows = self.cursor.fetchall()
         return rows
     
     def getServiceByServiceTypeID(self, service_type_id):
-        self.cursor.execute("SELECT * FROM service WHERE service_type_id = ? AND active = 1", (username,))
+        self.cursor.execute("SELECT * FROM service WHERE service_type_id = ? ", (service_type_id,))
         rows = self.cursor.fetchall()
         return rows
 
 
 
     def addAService(self, service_name, service_description, price, service_type_id):
-        self.cursor.execute("INSERT INTO serivce (service_name, service_description, price, service_type_id) VALUES (?, ?, ?, ?)",
+        self.cursor.execute("INSERT INTO service (service_name, service_description, price, service_type_id) VALUES (?, ?, ?, ?)",
                             (service_name, service_description, price, service_type_id))
         self.conn.commit()
 
